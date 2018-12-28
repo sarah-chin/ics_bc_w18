80ae91954b4b0c7364ee2c625c70a42858ee2036
sarah-chin <sarahchin@berkeley.edu>
2018-12-28 17:08:35 -0500

    Complete table_of_contents exercise

--- week1/ch06/table_of_contents.rb
+++ week1/ch06/table_of_contents.rb
@@ -4,9 +4,9 @@
 ### Your Code Here ###
 line_width = 50
 puts ("Table of Contents".center(line_width))
-puts ("Chapter 1: Getting Started" + "page 1".rjust(line_width/2))
-puts ("Chapter 2: Numbers" + "page 9".rjust(line_width/2))
-puts ("Chapter 3: Letters" + "page 13".rjust(line_width/2))
-puts ("Chapter 4: Variables and Assignment" + "page 17".rjust(line_width))
-puts ("Chapter 5: Mixing It Up" + "page 21".rjust(line_width))
-puts ("Chapter 6: More About Methods" + "page 27".rjust(line_width))
+puts ("Chapter 1: Getting Started" + "page 1".rjust(line_width-23))
+puts ("Chapter 2: Numbers" + "page 9".rjust(line_width - 15))
+puts ("Chapter 3: Letters" + "page 13".rjust(line_width - 14))
+puts ("Chapter 4: Variables and Assignment" + "page 17".rjust(line_width-31))
+puts ("Chapter 5: Mixing It Up" + "page 21".rjust(line_width-19))
+puts ("Chapter 6: More About Methods" + "page 27".rjust(line_width/2))
