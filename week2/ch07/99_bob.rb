77bf3efc826f391dabf14b61f74e45575643f9fd
sarah-chin <sarahchin@berkeley.edu>
2019-01-03 10:11:00 +0800

    99_bottles_of_beer

--- week2/ch07/99_bob.rb
+++ week2/ch07/99_bob.rb
@@ -3,3 +3,27 @@
 
 
 ### Your Code Here ###
+bottles = 99
+while bottles > 2
+	puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottles of beer.' #finish lyrics
+	bottles -= 1
+	puts 'Take one down and pass it around, ' + bottles.to_s + ' bottles of beer on the wall.'
+	puts 
+if bottles == 2
+	puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottles of beer.'
+	bottles -= 1
+	puts 'Take one down and pass it around, ' + bottles.to_s + ' bottle of beer on the wall.'
+	puts
+end	
+if bottles == 1
+	puts '1 bottle of beer on the wall, 1 bottle of beer.'
+	puts 'Take one down and pass it around, no more bottles of beer on the wall.'
+	bottles -= 1
+	puts
+end
+if bottles == 0
+	puts 'No more bottles of beer on the wall, no more bottles of beer.' 
+	puts 'Go to the store and buy some more, 99 bottles of beer on the wall.' 
+end
+end
+
