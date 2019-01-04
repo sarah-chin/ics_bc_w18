e780f2039d83044439efc5e4a67659f50f3cdacb
sarah-chin <sarahchin@berkeley.edu>
2019-01-04 20:19:19 +0800

    Deaf_Grandma Exercise

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -1,7 +1,16 @@
 puts 'COME GIVE GRANNY A KISS!'
+answer = gets.chomp
 
-while () # Fill in the while condition
+while (answer != answer.upcase && answer != 'BYE') # Fill in the while condition
   ### Your Code Here ###
+ 	puts 'HUH?! SPEAK UP, SONNY!'
+ 	answer = gets.chomp
 end
-
+while answer == answer.upcase
+	if answer == 'BYE'
+		break
+	end
+	puts 'NO, NOT SINCE ' + rand(1930..1950).to_s
+	answer = gets.chomp
+end
 puts 'OK SONNY, TALK AGAIN SOON'
