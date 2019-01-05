7cdfe5d935fad48d91eba4d01bb40c70be250d0b
sarah-chin <sarahchin@berkeley.edu>
2019-01-05 08:11:05 +0800

    Leap_Year_Exercise

--- week2/ch07/leap_year_counter.rb
+++ week2/ch07/leap_year_counter.rb
@@ -2,8 +2,22 @@
 
 ### Get input from user: ###
 print("Please enter a starting year: ")
-starting = gets.chomp ## FIXME?
-print("Now enter a ending year: ")
-ending = gets.chomp ## FIXME?
+starting = gets.chomp.to_i ## FIXME?
+print("Now enter an ending year: ")
+ending = gets.chomp.to_i ## FIXME?
 
 ### Rest of your code here ###
+counter = starting
+while counter >= starting && counter < ending
+	if counter%100 == 0 && counter%400 != 0
+		counter += 4
+	end
+	if counter%100 == 0 && counter%400 == 0
+		puts counter 
+		counter += 4
+	end
+	if counter%4 == 0
+		puts counter
+		counter += 4
+	end
+end
